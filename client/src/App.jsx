d708025050ad561cd729ed262bc610e23463879b
Awais Ahmad <aahmad19376@gmail.com>
2025-08-09 18:02:44 +0500

    First commit

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+import Header from './components/header';
+import HeroSection from './components/Home/hero-section';
+
+function App() {
+  
+
+  return (
+    <>
+       <Header/>
+
+       <HeroSection/>
+    </>
+  )
+}
+
+export default App
