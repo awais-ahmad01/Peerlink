9a261395eab21f07e4475b0fcefbef4f71939220
Awais Ahmad <aahmad19376@gmail.com>
2025-08-22 22:19:58 +0500

    updated..

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -14,7 +14,7 @@ async function registerUser(req, res) {
     try {
         
         const existingUser = await User.findOne({ email });
-        if (!existingUser) {
+        if (existingUser) {
             return res.status(400).json({ message: 'User already exists' });
         }
     
